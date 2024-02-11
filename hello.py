40132e1ba4bd299c76c91ba4b7421444986ed97a
velraaj18 <velraaj30@gmail.com>
2024-02-11 17:05:58 +0530

    Python

---
+++ hello.py
@@ -0,0 +1,8 @@
+def firstProgram():
+    print(True and False);  # and gives the first operand if its false or 0, Otherwise returns second operand.
+
+def main():
+    firstProgram();
+    
+if __name__ == "__main__":
+    main();
